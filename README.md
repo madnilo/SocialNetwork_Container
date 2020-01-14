53a850d2c90cd1308e3cf736cf01922dc54bc200
Danilo Moura <danilo.moura@ptml.com>
2020-01-14 19:36:54 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Container APP
+
+This is the container app of this micro-frontend approach project.
+
+As a Container app, it will be responsible for providing:
+- Common dependencies
+- A styled viewport / layout in which the micro apps will mount
+- What else?
+
+### Container App Dependencies (need some tree shaking but will do the trick for now)
+```
+// dev dependencies
+npm i --save-dev @babel/cli @babel/core @babel/plugin-proposal-class-properties @babel/preset-env @babel/preset-react babel-loader file-loader form-urlencoded html-loader html-webpack-plugin https-proxy-agent image-webpack-loader prettier pretty-quick prop-types querystring socket.io uuid webpack webpack-cli webpack-dev-server webpack-merge
+
+// common dependencies
+npm i --save @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome axios client-oauth2 react react-dom react-redux react-router react-router-dom redux redux-thunk styled-components
+```
+
+
+
+### Internal micro apps dependencies
+```
+// todo ???
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "js/[name].js",
+    publicPath: "/",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    library: "[name]",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx", ".scss"],
+  },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
